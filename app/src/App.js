2c40234d9dc01cbd982dd517efb79599e781fc1d
cyhwill <cuo2017@163.com>
2018-12-30 04:37:19 -0800

    first commit

---
+++ app/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Application from './components/index';
+import './App.css';
+
+const App = () => (
+  <Application />
+);
+
+
+
+export default App;
